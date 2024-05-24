4eabc21142f5fd886f7c038bef4a87ef06e65da6
luukhd2 <86712767+luukhd2@users.noreply.github.com>
2024-05-24 16:23:05 +0200

    add AF2 LIS and roc plots

--- classifier_benchmark/roc.py
+++ classifier_benchmark/roc.py
@@ -1,153 +1,125 @@
-"""Receiver operating characteristic for the classifier benchmark."""
-
-import sys
 import pathlib
-import os
-
 import pandas as pd
 import numpy as np
+import seaborn as sns
 import matplotlib.pyplot as plt
+import logging
+
 from sklearn.metrics import roc_curve, auc
-
-from parse_predictions import get_models
-
-
-def get_ground_truth(
-    path_to_decoy_csv,
-    interaction_column="Acts as agonist",
-    decoy_type_column="Decoy Type",
-    identifier_column="identifier",
-    # for creating the identifier if it isn't found
-    receptor_col="Target ID",
-    peptide_col="Decoy ID",
-):
-    """Get a pandas df for the ground truth of the decoys interactions."""
-    df = pd.read_csv(path_to_decoy_csv)
-
-    # check if identifier column exists
-    if identifier_column not in df.columns:
-        print(
-            f"Identifier column not found in the decoy csv. Creating one from {receptor_col} and {peptide_col}..."
-        )
-        df[identifier_column] = (
-            df[receptor_col].astype(str) + "___" + df[peptide_col].astype(str)
-        )
-    assert df[identifier_column].is_unique, "Identifier column is not unique"
-    # make interaction column boolean
-    df[interaction_column] = df[interaction_column].astype(bool)
-    # make decoy type column categorical
-    df[decoy_type_column] = df[decoy_type_column].astype("category")
-    return df
+from parse_predictions import get_models, get_ground_truth_df, get_ground_truth_values
 
 
-def plot_ROC_curve(
-    truth_df,
-    prediction_list,
-    save_path,
-    identifier_column="identifier",
-    interaction_column="Acts as agonist",
-    prediction_column="InteractionProbability",
-    remove_similar=False,
-):
+def create_roc(invalid_identifiers, plot_p, log_p):
     """
-    Plot the ROC curve for the models.
-    use roc_curve and auc from sklearn.metrics
-    add the AUC to the legend
-
-    match the rows based on the identifier column
+    valid_identifiers: list of str, identifiers to keep. Remove other ones
     """
-    if remove_similar:
-        print("Removing similar decoys.")
-        truth_df = truth_df[truth_df["Decoy Type"] != "Similar"]
+    # settings
+    script_dir = pathlib.Path(__file__).parent
+    model_dir = script_dir / "models"
+    identifier_col = "identifier"
+    score_col = "InteractionProbability"
 
-    pos_identifiers = truth_df[truth_df[interaction_column]][identifier_column]
-    neg_identifiers = truth_df[~truth_df[interaction_column]][identifier_column]
-    print("Positive identifiers:", len(pos_identifiers))
-    print("Negative identifiers:", len(neg_identifiers))
+    # run
+    logging.basicConfig(filename=log_p, level=logging.INFO, filemode="w")
+    models = get_models(model_dir)
+    ground_truth = get_ground_truth_df()
+    # filter invalid identifiers
+    ground_truth = ground_truth[~ground_truth[identifier_col].isin(invalid_identifiers)]
+    number_positives = len(ground_truth[ground_truth["Acts as agonist"] == 1])
+    number_negatives = len(ground_truth[ground_truth["Acts as agonist"] == 0])
 
-    # plot
-    plt.figure()
-    plt.plot([0, 1], [0, 1], "k--")
-    for model_name, prediction_df in prediction_list:
-        print("Plotting ROC curve for", model_name)
-        if remove_similar:
-            # keep only identifiers in truth_df
-            prediction_df = prediction_df[
-                prediction_df[identifier_column].isin(truth_df[identifier_column])
-            ]
-        if not len(prediction_df) == len(truth_df):
-            print(len(prediction_df), len(truth_df))
-            raise ValueError("Prediction and truth dataframes are not the same length.")
+    # empty fig
+    fig, ax = plt.subplots()
 
-        # match the rows based on the identifier column
-        prediction_df = prediction_df.set_index(identifier_column)
-        pos_predictions = prediction_df.loc[pos_identifiers][prediction_column]
-        neg_predictions = prediction_df.loc[neg_identifiers][prediction_column]
+    # iterate over models
+    for model_name, prediction_df in models:
+        logging.info(f"Processing model {model_name}")
+        logging.info(f"Number of predictions: {len(prediction_df)}")
+        nan_count = len(prediction_df) - len(prediction_df.dropna(subset=[score_col]))
+        prediction_df = prediction_df.dropna(subset=[score_col])
+        logging.info(f"Number of dropped NaNs in predictions: {nan_count}")
+        # lastly, remove the identifiers that are not used in this evaluation
+        prediction_df = prediction_df[
+            ~prediction_df[identifier_col].isin(invalid_identifiers)
+        ]
+        logging.info(f"Number of predictions after filtering: {len(prediction_df)}")
+
+        y_pred = prediction_df[score_col].to_numpy()
+        y_true = get_ground_truth_values(ground_truth, prediction_df[identifier_col])
+
+        if len(y_pred) != len(ground_truth):
+            logging.warning(
+                f"Number of predictions ({len(y_pred)}) does not match the number of ground truth values ({len(ground_truth)})"
+            )
 
         # calculate ROC curve
-        y_true = np.concatenate(
-            [np.ones_like(pos_predictions), np.zeros_like(neg_predictions)]
-        )
-        y_pred = np.concatenate([pos_predictions, neg_predictions])
-
-        # get nan indexes in y_pred
-        nan_idx = np.isnan(y_pred)
-        if np.any(nan_idx):
-            nan_count = np.sum(nan_idx)
-            print(f"Found {nan_count} NaN values in the predictions.")
-            y_true = y_true[~nan_idx]
-            y_pred = y_pred[~nan_idx]
-            label_start = f"{model_name}\n{nan_count} NaN values removed.\n"
-        else:
-            label_start = f"{model_name}"
-
         fpr, tpr, _ = roc_curve(y_true, y_pred)
         roc_auc = auc(fpr, tpr)
-        plt.plot(fpr, tpr, label=f"{label_start} (AUC = {roc_auc:.2f})")
+        label = f"{model_name}\nAUC = {roc_auc:.2f}"
+        missing_values = len(ground_truth) - len(y_pred)
+        if missing_values > 0:
+            label += f"\n({missing_values} missing values)"
 
-    plt.xlabel("False Positive Rate")
-    plt.ylabel("True Positive Rate")
-    plt.title("ROC Curve, classifier benchmark")
-    plt.legend(loc="lower right")
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.0])
-    # add number of positive and negative interactions
-    sample_label = f"n_pos={len(pos_identifiers)}\nn_neg={len(neg_identifiers)}"
-    plt.text(0.01, 0.99, sample_label, ha="left", va="top")
+        ax.plot(
+            fpr,
+            tpr,
+            label=label,
+        )
 
-    plt.savefig(save_path, dpi=300)
+    # make plot informative
+    ax.plot([0, 1], [0, 1], color="navy", lw=2, linestyle="--")
+    ax.set_xlabel("False Positive Rate")
+    ax.set_ylabel("True Positive Rate")
+    ax.set_title("ROC Curve")
+    ax.legend(loc="lower right")
+    ax.set_xlim([0.0, 1.0])
+    ax.set_ylim([0.0, 1.0])
+    plt.title(
+        f"Classifier performance\n n={len(ground_truth)}, ({number_positives} +, {number_negatives} -)"
+    )
+    # adjust legend font dict
+    for text in ax.get_legend().get_texts():
+        text.set_fontsize("small")
+    plt.savefig(plot_p, dpi=300)
+    logging.info(f"Wrote ROC curve to {plot_p}")
+
+
+def main():
+    ground_truth = get_ground_truth_df()
+    script_dir = pathlib.Path(__file__).parent
+
+    # create roc for all
+    create_roc(
+        invalid_identifiers=[],
+        plot_p=script_dir / "plots/roc_all.png",
+        log_p=script_dir / "plots/roc_all.log",
+    )
+
+    # create roc WITHOUT similar decoys
+    similar_identifiers = list()
+    for i, row in ground_truth.iterrows():
+        if row["Decoy Type"] == "Similar":
+            similar_identifiers.append(row["identifier"])
+    create_roc(
+        invalid_identifiers=similar_identifiers,
+        plot_p=script_dir / "plots/roc_no_similar.png",
+        log_p=script_dir / "plots/roc_no_similar.log",
+    )
+
+    # create roc for ONLY most dissimilar
+    not_included = list()
+    for i, row in ground_truth.iterrows():
+        if row["Decoy Type"] == "Similar":
+            not_included.append(row["identifier"])
+        elif row["Decoy Rank"] > 0:
+            not_included.append(row["identifier"])
+
+    create_roc(
+        invalid_identifiers=not_included,
+        plot_p=script_dir / "plots/roc_most_dissimilar.png",
+        log_p=script_dir / "plots/roc_most_dissimilar.log",
+    )
 
 
 if __name__ == "__main__":
-    # settings
-    SCRIPT_DIR = pathlib.Path(__file__).resolve().parent
-    MODEL_DIR = SCRIPT_DIR / "models"
-    DECOY_CSV_P = (
-        SCRIPT_DIR.parent
-        / "classifier_benchmark_data/output/6_interactions_with_decoys.csv"
-    )
-    PLOT_PATH = SCRIPT_DIR / "roc_curve.png"
-
-    PREDICTION_COLUMN = "InteractionProbability"
-    INTERACTION_COLUMN = "Acts as agonist"
-
-    # get the model names, and prediction DFs
-    TRUTH_DF = get_ground_truth(DECOY_CSV_P)
-    PREDICTION_LIST = get_models(MODEL_DIR)
-    plot_ROC_curve(
-        TRUTH_DF,
-        PREDICTION_LIST,
-        SCRIPT_DIR / "plots/roc_curve.png",
-        interaction_column=INTERACTION_COLUMN,
-        prediction_column=PREDICTION_COLUMN,
-    )
-
-    # remove similar decoys
-    plot_ROC_curve(
-        TRUTH_DF,
-        PREDICTION_LIST,
-        SCRIPT_DIR / "plots/roc_curve_no_similar.png",
-        interaction_column=INTERACTION_COLUMN,
-        prediction_column=PREDICTION_COLUMN,
-        remove_similar=True,
-    )
+    main()
