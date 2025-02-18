22572fa7d46e7c304afe94dcae209c0f4d315ea3
Teemu Rönkkö <31726090+teemuronkko@users.noreply.github.com>
2025-02-18 15:33:45 -0500

    Added multimer json script

--- classifier_benchmark/models/AF3/make_monomer_af3_jsons.py
+++ classifier_benchmark/models/AF3/make_monomer_af3_jsons.py
@@ -13,43 +13,43 @@ repo_dir = file_dir[:index + len(repo_name)]
 decoy_df = pd.read_csv(f"{repo_dir}/classifier_benchmark_data/output/6_interactions_with_decoys.csv")
 
 # Make a directory for the jsons
-os.makedirs(f"{repo_dir}/classifier_benchmark/models/AF3/classifier_jsons", exist_ok=True)
-os.makedirs(f"{repo_dir}/classifier_benchmark/models/AF3/classifier_jsons/receptors", exist_ok=True)
-os.makedirs(f"{repo_dir}/classifier_benchmark/models/AF3/classifier_jsons/peptides", exist_ok=True)
+json_folder = f"{repo_dir}/classifier_benchmark/models/AF3/classifier_jsons"
+os.makedirs(f"{json_folder}", exist_ok=True)
+os.makedirs(f"{json_folder}/receptors", exist_ok=True)
+os.makedirs(f"{json_folder}/peptides", exist_ok=True)
 
-# json name: Identifier + .json
 for index, row in decoy_df.iterrows():
     # Parse id and sequences
     ligand_sequence = row["Ligand Sequence"]
     gpcr_sequence = row["GPCR Sequence"]
     identifier = row["Identifier"]
-    
     gpcr_name = identifier.split("___")[0]
     peptide_name = identifier.split("___")[1]
 
-    gpcr_msa_path = f"/projects/ilfgrid/people/pqh443/AF3/MSAs/{gpcr_name}/"
-    peptide_msa_path = f"/projects/ilfgrid/people/pqh443/AF3/MSAs/{peptide_name}/"
+    # Paths to the jsons
+    receptor_json_path = f"{json_folder}/receptors/{gpcr_name}.json"
+    peptide_json_path = f"{json_folder}/peptides/{peptide_name}.json"
 
     # Make json for the receptor only
     receptor_dict = {}
-    receptor_dict["name"] = identifier.split("___")[0]
+    receptor_dict["name"] = gpcr_name
     receptor_dict["sequences"] = []
     receptor_dict["sequences"].append({"protein": {"id": "A", "sequence": gpcr_sequence}})
     receptor_dict["modelSeeds"] = [1]
     receptor_dict["dialect"] = "alphafold3"
     receptor_dict["version"] = 1
-    if not os.path.exists(f"{repo_dir}/classifier_benchmark/models/AF3/classifier_jsons/receptors/{receptor_dict['name']}.json"):
-        with open(f"{repo_dir}/classifier_benchmark/models/AF3/classifier_jsons/receptors/{receptor_dict['name']}.json", "w") as f:
+    if not os.path.exists(receptor_json_path):
+        with open(receptor_json_path, "w") as f:
             json.dump(receptor_dict, f, indent=2)
 
     # Make json for the peptide only
     peptide_dict = {}
-    peptide_dict["name"] = identifier.split("___")[1]
+    peptide_dict["name"] = peptide_name
     peptide_dict["sequences"] = []
     peptide_dict["sequences"].append({"protein": {"id": "B", "sequence": ligand_sequence}})
     peptide_dict["modelSeeds"] = [1]
     peptide_dict["dialect"] = "alphafold3"
     peptide_dict["version"] = 1
-    if not os.path.exists(f"{repo_dir}/classifier_benchmark/models/AF3/classifier_jsons/peptides/{peptide_dict['name']}.json"):
-        with open(f"{repo_dir}/classifier_benchmark/models/AF3/classifier_jsons/peptides/{peptide_dict['name']}.json", "w") as f:
+    if not os.path.exists(peptide_json_path):
+        with open(peptide_json_path, "w") as f:
             json.dump(peptide_dict, f, indent=2)
