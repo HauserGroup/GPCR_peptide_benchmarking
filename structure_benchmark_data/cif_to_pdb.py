827b560a9feee83f285ad52bc2b2c044161ee995
Teemu Rönkkö <31726090+teemuronkko@users.noreply.github.com>
2025-02-28 15:05:25 -0500

    Added converted files (cif to pdb)

--- structure_benchmark_data/cif_to_pdb.py
+++ structure_benchmark_data/cif_to_pdb.py
@@ -1,4 +1,8 @@
 import os
+import glob 
+import numpy as np 
+import pandas as pd
+
 # Helper script to produce pymol command line commands to convert cif files to pdb files
 # Used mainly for large input files that cannot be downloaded from the RCSB website and AF3 models
 
@@ -15,8 +19,8 @@ index = script_dir.find(repo_name)
 repo_dir = script_dir[:index + len(repo_name)]
 
 # Input and output directories
-cif_dir = f"{repo_dir}/structure_benchmark_data/updated_data_Feb2025/cifs"
-pdb_dir = f"{repo_dir}/structure_benchmark_data/updated_data_Feb2025/pdbs"
+cif_dir = f"{repo_dir}/structure_benchmark_data/cifs"
+pdb_dir = f"{repo_dir}/structure_benchmark_data/pdbs"
 
 # Convert all cif files to pdb files
 for cif_file in os.listdir(cif_dir):
@@ -25,3 +29,54 @@ for cif_file in os.listdir(cif_dir):
         cif_file = f"{cif_dir}/{cif_file}"
         pdb_file = f"{pdb_dir}/{pdb_file}"
         cif_to_pdb(cif_file, pdb_file)
+
+# Convert AF3 models to pdb files
+AF3_server_path = f"{repo_dir}/structure_benchmark/AF3_server"
+
+AF3_models = glob.glob(f"{AF3_server_path}/*/*_model_0.cif")
+
+# List all _model_0.cif files in the AF3 server directory
+for cif_file in AF3_models:
+    pdb_id = cif_file.split("/")[-2].upper()
+    pdb_file = f"{AF3_server_path}/{pdb_id}.pdb"
+    cif_to_pdb(cif_file, pdb_file)
+
+# Convert AF3 models to pdb files
+AF3_path = f"{repo_dir}/structure_benchmark/AF3"
+
+AF3_models = glob.glob(f"{AF3_path}/*/*_model.cif")
+for cif_file in AF3_models:
+    pdb_id = cif_file.split("/")[-2].upper()
+    pdb_file = f"{AF3_path}/{pdb_id}.pdb"
+    #print(pdb_file, cif_file)
+    cif_to_pdb(cif_file, pdb_file)
+
+# Convert AF3 models to pdb files
+AF3_no_templates_path = f"{repo_dir}/structure_benchmark/AF3_no_templates"
+AF3_models = glob.glob(f"{AF3_no_templates_path}/*/*_model.cif")
+for cif_file in AF3_models:
+    pdb_id = cif_file.split("/")[-2].upper().replace("_NO_TEMPLATES", "")
+    pdb_file = f"{AF3_no_templates_path}/{pdb_id}.pdb"
+    cif_to_pdb(cif_file, pdb_file)
+
+# Read npz files from folder
+npz_dir = f"{repo_dir}/structure_benchmark/Chai-1/"
+npz_files = glob.glob(f"{npz_dir}/*/*.npz")
+npz_scores = []
+for npz_file in npz_files:
+    model_id = npz_file.split("/")[-2]
+    model_name = npz_file.split("/")[-1]
+    # Read npz file
+    npz = np.load(npz_file)
+    # To dictionary
+    npz_dict = dict(npz)
+    agg_score = npz_dict["aggregate_score"].item()
+    ptm = npz_dict["ptm"].item()
+    iptm = npz_dict["iptm"].item()
+    npz_scores.append([model_id, model_name, agg_score, ptm, iptm])
+
+chai_results_df = pd.DataFrame(npz_scores, columns=["model_id", "model_name", "aggregate_score", "ptm", "iptm"])
+chai_results_df = chai_results_df.sort_values(["model_id", "iptm"], ascending=[True, False])
+chai_results_df = chai_results_df.drop_duplicates(subset="model_id", keep="first")
+
+print(chai_results_df)
--- structure_benchmark_data/subanalyses/runtime_analysis.py
+++ structure_benchmark_data/subanalyses/runtime_analysis.py
@@ -3,6 +3,12 @@ import pandas as pd
 import os
 import re
 import sys 
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy.stats import wilcoxon
+import itertools
+from statsmodels.stats.multitest import multipletests
+import numpy as np
 
 file_dir = os.getcwd()
 repo_name = "GPCR_peptide_benchmarking"
@@ -113,6 +119,83 @@ def parse_alphafold3_runtime(folder_path):
     
     return runtime_dict
 
+def round_to_significant_digits(number, significant_digits=3):
+    if pd.isnull(number) or not isinstance(number, (int, float)):
+        return number
+    if number == 0:
+        return 0
+    else:
+        return round(number, significant_digits - int(np.floor(np.log10(abs(number)))) - 1)
+
+def significance_stars(p):
+    if p < 0.0001:
+        return '****'
+    elif p < 0.001:
+        return '***'
+    elif p < 0.01:
+        return '**'
+    elif p < 0.05:
+        return '*'
+    else:
+        return 'ns'
+
+def get_significance(data, variable, model_col='model', pdb_col='pdb'):
+    # Order dataframe by model and then by PDB code
+    data = data.sort_values(by=[model_col, pdb_col])
+
+    # Perform pairwise Wilcoxon signed-rank tests between all models
+    results = []
+    for model1, model2 in itertools.combinations(data[model_col].unique(), 2):
+        # Get subset of data for each model
+        var1 = data[data[model_col] == model1][[variable, pdb_col, model_col]].dropna()
+        var2 = data[data[model_col] == model2][[variable, pdb_col, model_col]].dropna()
+
+        # Merge the two subsets on PDB code
+        merged = var1.merge(var2, on=pdb_col, suffixes=(f'_{model1}', f'_{model2}'))
+
+        if merged.shape[0] < 2:
+            # Skip if there are not enough data points for the test
+            continue
+
+        try:
+            stat, p_value = wilcoxon(merged[f'{variable}_{model1}'], merged[f'{variable}_{model2}'])
+        except ValueError:
+            # Skip this pair if Wilcoxon test fails
+            continue
+
+        # Calculate averages and standard errors
+        avg_1 = var1[variable].mean()
+        se_1 = var1[variable].sem()
+        avg_2 = var2[variable].mean()
+        se_2 = var2[variable].sem()
+
+        results.append((model1, model2, avg_1, se_1, avg_2, se_2, stat, p_value))
+
+    # Create a DataFrame from the results
+    results_df = pd.DataFrame(results, columns=[
+        'Model1', 'Model2', f'Model1_Avg_{variable}', f'Model1_SE_{variable}', 
+        f'Model2_Avg_{variable}', f'Model2_SE_{variable}', 'Statistic', 'P-value'
+    ])
+
+    # Apply Benjamini-Hochberg correction and add corrected p-values to the DataFrame
+    if not results_df.empty:
+        _, corrected_p_values, _, _ = multipletests(results_df['P-value'], method='fdr_bh')
+        results_df['Corrected P-value'] = corrected_p_values
+
+        # Round the p-values and apply the function to create a new column with significance stars
+        results_df['P-value'] = results_df['P-value'].apply(lambda x: round(x, 10))
+        results_df['Corrected P-value'] = results_df['Corrected P-value'].apply(lambda x: round(x, 10))
+        results_df['Significance'] = results_df['Corrected P-value'].apply(significance_stars)
+
+        # Apply function to all numeric columns in the DataFrame
+        for col in results_df.columns:
+            results_df[col] = results_df[col].apply(lambda x: round_to_significant_digits(x, 4) if pd.to_numeric(x, errors='coerce') is not None else x)
+
+        # Save the results to a CSV file
+        results_df.to_csv(f'{repo_dir}/structure_benchmark_data/wilcoxon_results_{variable}.csv', index=False)
+    else:
+        print(f"No valid comparisons for {variable}")
+
 rfaa_runtimes = parse_esm_rfaa_runtimes(rfaa_runtime_path, rfaa_model_path)
 rfaa_no_templates = {k: v for k, v in rfaa_runtimes.items() if "no_templates" in k}
 rfaa_with_templates = {k: v for k, v in rfaa_runtimes.items() if "no_templates" not in k}
@@ -140,11 +223,7 @@ af3_no_templates_df["Model Type"] = "AF3 (no templates)"
 # Concatenate the dataframes
 all_runtimes = pd.concat([rfaa_df, rfaa_no_templates_df, esm_df, chai_df, af3_df, af3_no_templates_df])
 
-print(all_runtimes)
-
-# Plot the runtimes
-import seaborn as sns
-import matplotlib.pyplot as plt
+### Plot the runtimes
 
 # Get consistent order
 model_order = all_runtimes["Model Type"].unique()
@@ -185,9 +264,8 @@ sns.swarmplot(
 
 plt.rcParams['svg.fonttype'] = 'none'
 plt.ylim(0, 500)
-plt.title("Runtime comparison between different models\n")
+plt.title("Runtime comparison between different models")
 ax.set_xlabel("")
-
 plt.ylabel("Runtime (seconds)", fontsize="large")
 plt.xticks(rotation=90)
 
@@ -201,3 +279,12 @@ ax.tick_params(axis='y', direction='in')
 plt.tight_layout()
 plt.savefig(f"{repo_dir}/structure_benchmark_data/plots/runtime_comparison.svg", format="svg")
 plt.close()
+
+# Statistical analysis of the runtimes
+all_runtimes["Model"] = all_runtimes["Model"].str.upper()
+all_runtimes["Model"] = [i.split("_")[0] for i in all_runtimes["Model"]]
+all_runtimes.to_csv(f'{repo_dir}/structure_benchmark_data/model_runtimes.csv', index=False)
+
+# Perform Wilcoxon signed-rank tests for DockQ scores
+all_runtimes = all_runtimes.rename(columns={"Model": "pdb", "Model Type": "model", "Runtime": "Runtime"})
+get_significance(all_runtimes, 'Runtime')
